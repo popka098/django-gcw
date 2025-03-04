b12ebc3178fb93202593309407b03d6c6f2b3e36
pop_ka <andrey.petrovich0880@gmail.com>
2025-03-04 22:10:12 +0300

    some changes and notes

--- main/templates/pages/training/js/training.js
+++ main/templates/pages/training/js/training.js
@@ -1,59 +1,60 @@
-
-
-
-
-function isLetter(str) 
-{
+function isLetter(str) {
     return str.length === 1 && str.match(/[а-я]/i);
 }
 
 var el_compl = document.getElementById("compl"); // элемент с пройденными словами
 var el_next = document.getElementById("next"); // элемент с предстоящими словами
-var el_context_bef = document.getElementById("context_after"); // элемент с контекстом перед словом
-var el_context_aft = document.getElementById("context_before"); // элемент с контекстом после слова
 
 const words = [
     {
-        "Word": "фланелевый",
-        "Context_Before": "",
-        "Pass": "фланел.вый",
-        "Context_After": "",
+        Word: "фланелевый",
+        Context_Before: "",
+        Pass: "фланел.вый",
+        Context_After: "",
     },
     {
-        "Word": "ситечко",
-        "Context_Before": "чайное",
-        "Pass": "сит.чко",
-        "Context_After": "",
+        Word: "ситечко",
+        Context_Before: "чайное",
+        Pass: "сит.чко",
+        Context_After: "",
     },
     {
-        "Word": "ветраяная",
-        "Context_Before": "",
-        "Pass": "ветр.ная",
-        "Context_After": "мельница",
+        Word: "ветраяная",
+        Context_Before: "",
+        Pass: "ветр.ная",
+        Context_After: "мельница",
     },
 ]; // тестовые слова
-
-var rand_item = words[Math.floor(Math.random()*words.length)]; // рандом
-console.log(rand_item);
-
+const char_amount = 50;
 
 var next_words = [];
-for (var i = 0; i < 10; i++)
-{
-    next_words.push(words[Math.floor(Math.random()*words.length)]);
+for (var i = 0; i < 10; i++) {
+    next_words.push(words[Math.floor(Math.random() * words.length)]);
 }
 
 window.addEventListener("keydown", run);
 var compl = "";
-function run(e)
-{
+var cur_compl = "";
+function run(e) {
     const key = e.key;
-    if (!(isLetter(key) || (key == " ") || (e.code == "Backspace"))) // проверка на необходимый символ
-    {
+    if (!(isLetter(key) || key == " " || e.code == "Backspace")) {
+        // проверка на необходимый символ
         return;
     }
-    
+
+    if (key == " ") {
+        // проверка введенного слова cur_compl на правильность
+        return;
+    }
+
+    if (e.code == "Backspace") {
+        // удаление символов
+        return;
+    }
+
+    // сделать ограничение возможного ввода при предопределенных символах (символы кроме .)
     compl += key;
+    cur_compl += key;
     console.log(compl);
     el_compl.innerHTML = compl;
 }
