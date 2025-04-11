2209458e278b50a6e3daaffb85f0f8ee63aa53a4
pop_ka <andrey.petrovich0880@gmail.com>
2025-04-11 14:25:41 +0300

    added getting list of random words from task

--- main/urls_api.py
+++ main/urls_api.py
@@ -8,10 +8,11 @@ import training.views as training_views
 from django.conf import settings
 from django.conf.urls.static import static
 
-from main.views_api import get_all_words, serializater_testing
+from main.views_api import get_all_words, serializater_testing, get_random_words
 
 urlpatterns = [
     path("get_all/<int:limit>", get_all_words),
     path("test", serializater_testing),
+    path("get_words/<int:task>/<int:limit>", get_random_words),
 ]
 
--- main/views_api.py
+++ main/views_api.py
@@ -3,9 +3,17 @@ import random
 from django.http import JsonResponse
 from django.core.handlers.wsgi import WSGIRequest
 
+from rest_framework.exceptions import bad_request
+
 from training.models import Task_9, Task_10, Task_11, Task_12
 from training.models import TaskSerializer, WordsSerializer
 
+tasks = {
+    9: Task_9,
+    10: Task_10,
+    11: Task_11,
+    12: Task_12
+}
 
 
 def get_all_words(request: WSGIRequest, limit=0):
@@ -24,8 +32,23 @@ def get_all_words(request: WSGIRequest, limit=0):
     return JsonResponse({"words": words_shuffeled[:limit]})
 
 
+def get_random_words(request: WSGIRequest, task=9, limit=0):
+    if not(9 <= task <= 12):
+        return bad_request(request, "No such table")
+    
+
+    words = WordsSerializer(tasks[task].objects.all()).data
+    if limit == 0:
+        limit = len(words)
+
+    words_shuffeled = words.copy()
+    random.shuffle(words_shuffeled)
+    return JsonResponse({"words": words_shuffeled[:limit]})
+
+
 def serializater_testing(request: WSGIRequest):
     words = Task_11.objects.all()
     ser = WordsSerializer(words)
     return JsonResponse({"words": ser.data})
+
 
