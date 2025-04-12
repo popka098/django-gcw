49915aae3c7267d0be90d9f8c6deec6cd2a04e7f
Nikita <my@example.com>
2025-04-12 18:49:05 +0300

    for subscribers

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from functools import wraps
 
 
 def index_page(request: WSGIRequest):
@@ -9,11 +11,21 @@ def index_page(request: WSGIRequest):
     return render(request, 'pages/index.html', context)
 
 
+def subscription_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_authenticated and hasattr(request.user, 'subscription') and request.user.subscription.is_active:
+            return view_func(request, *args, **kwargs)
+        return HttpResponseForbidden("У вас нет доступа к этой странице. Пожалуйста, оформите подписку.")
+    return _wrapped_view
+
 def login_page(request: WSGIRequest):
     raise NotImplementedError
 
 
 def registration_page(request: WSGIRequest):
     raise NotImplementedError
+
+@subscription_required
 def theory_page(request: WSGIRequest):
     return render(request, 'pages/theory.html')
