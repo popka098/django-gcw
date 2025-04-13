d4f15bf83cfe14e478a7e4260b7049b914e0c30c
Половинкин Артем <darmioniks@gmail.com>
2025-04-13 12:58:14 +0300

    nightmare

--- main/models.py
+++ main/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.db import models
 from django.contrib.auth.models import User
+
 
 class Subscription(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=False)
-
-
 class Profile(models.Model):
     """
     Доп. таблица к пользователю
--- main/views.py
+++ main/views.py
@@ -1,22 +1,41 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
 from django.http import HttpResponseForbidden
 from functools import wraps
+from .models import Subscription
+
 
 
 def index_page(request: WSGIRequest):
     context = {
         'pagename': "Главная"
     }
+    user = authenticate(username="admin", password="admin")
+    if user:
+        login(request, user)
+        context["is_login"] = True
+        return render(request, 'pages/index.html', context)
+
     return render(request, 'pages/index.html', context)
 
 
 def subscription_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        if request.user.is_authenticated and hasattr(request.user, 'subscription') and request.user.subscription.is_active:
-            return view_func(request, *args, **kwargs)
+        if request.user.is_authenticated:
+            if hasattr(request.user, 'subscription'):
+                if request.user.subscription.is_active:
+                    return view_func(request, *args, **kwargs)
+                else:
+                    print("Подписка неактивна")
+            else:
+                print("Подписка не найдена")
+        else:
+            print("Пользователь не аутентифицирован")
         return HttpResponseForbidden("У вас нет доступа к этой странице. Пожалуйста, оформите подписку.")
+
     return _wrapped_view
 
 def login_page(request: WSGIRequest):
@@ -25,6 +44,7 @@ def login_page(request: WSGIRequest):
 
 def registration_page(request: WSGIRequest):
     raise NotImplementedError
+
 
 @subscription_required
 def theory_page(request: WSGIRequest):
