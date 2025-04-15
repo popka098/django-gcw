98ad8835d91b72f83e504696f496c4ea16cc83a9
Nikolay <kolyakartechev@gmail.com>
2025-04-15 20:39:40 +0300

    Добавил адрес страницы статистики в конфиг

--- GCW/urls.py
+++ GCW/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 from main import views as main_views
 from django.contrib.auth import views as auth_views
+from training import views as training_views
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -14,6 +15,7 @@ urlpatterns = [
     path('registration/', main_views.registration_page, name='registration'),
     path('login/', main_views.login_page, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('stats/', training_views.statistics_page, name='statistics'),
 
 ]
 urlpatterns += static(settings.MEDIA_URL,
