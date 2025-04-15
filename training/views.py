e8439076c9e229ee5972214142be460ea42b4a0b
Nikolay <kolyakartechev@gmail.com>
2025-04-15 21:03:44 +0300

    Итоговая версия, исправил недочеты, подправил вывод в процентах.

--- training/views.py
+++ training/views.py
@@ -3,14 +3,16 @@ from django.core.handlers.wsgi import WSGIRequest
 from training.models import Stats, Atts
 
 
+
 def statistics_page(request: WSGIRequest):
     stat = Stats.objects.get(user=request.user)
     attemps = Atts.objects.filter(user=request.user)
+    print(100*(stat.successes // (stat.successes + stat.mistakes)))
     context = {
         "time_all" : stat.time,
         "success_all" : stat.successes,
         "mistakes_all" : stat.mistakes,
-        "kd" : 0 if stat.successes == 0 and stat.mistakes == 0 else 100*(stat.successes // (stat.successes + stat.mistakes)),
+        "kd" : 0 if (stat.successes == 0 and stat.mistakes == 0) else int(100*(stat.successes / (stat.successes + stat.mistakes))),
         "attemps": attemps,
     }
-    return render(request, "pages/statistics/general_statistic.html", context)
+    return render(request, "../../training/templates/pages/general_statistics.html", context)
