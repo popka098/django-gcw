33b9a700cb1c0bcdf4571c11ec1d3a276a5dcecf
Nikolay <kolyakartechev@gmail.com>
2025-04-15 20:38:12 +0300

    Сделан функционал страницы статистики

--- training/views.py
+++ training/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.core.handlers.wsgi import WSGIRequest
+from training.models import Stats, Atts
 
 
-def stats(request: WSGIRequest):
+def statistics_page(request: WSGIRequest):
+    stat = Stats.objects.get(user=request.user)
+    attemps = Atts.objects.filter(user=request.user)
     context = {
+        "time_all" : stat.time,
+        "success_all" : stat.successes,
+        "mistakes_all" : stat.mistakes,
+        "kd" : 0 if stat.successes == 0 and stat.mistakes == 0 else 100*(stat.successes // (stat.successes + stat.mistakes)),
+        "attemps": attemps,
     }
-    return 0
+    return render(request, "pages/statistics/general_statistic.html", context)
