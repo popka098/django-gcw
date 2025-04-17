9dedc0b1290fb0f1cbc209bfb59a21e7241374cc
Andrey Petrov <andrey.petrovich0880@gmail.com>
2025-04-17 20:48:30 +0300

    added validation for importing data

--- training/admin.py
+++ training/admin.py
@@ -9,37 +9,101 @@ import training.models as models
 class Task_9_Resource(resources.ModelResource):
     class Meta:
         model = models.Task_9
+    
+    
+    def skip_row(self, instance, original, row, import_validation_errors=None):
+        _Word = row["Word"]
+        _Pass = row["Pass"]
+        print(_Word, _Pass)
+        if (
+            len(_Word) == 0 or 
+            len(_Pass) == 0 or 
+            len(_Word) != len(_Pass) or
+            _Pass.count(".") != 1
+        ):
+            return True
+        return False
+            
+
 class Task_10_Resource(resources.ModelResource):
     class Meta:
         model = models.Task_10
+
+    
+    def skip_row(self, instance, original, row, import_validation_errors=None):
+        _Word = row["Word"]
+        _Pass = row["Pass"]
+        print(_Word, _Pass)
+        if (
+            len(_Word) == 0 or 
+            len(_Pass) == 0 or 
+            len(_Word) != len(_Pass) or
+            _Pass.count(".") != 1
+        ):
+            return True
+        return False
 
 class Task_11_Resource(resources.ModelResource):
     class Meta:
         model = models.Task_11
 
+    
+    def skip_row(self, instance, original, row, import_validation_errors=None):
+        _Word = row["Word"]
+        _Pass = row["Pass"]
+        print(_Word, _Pass)
+        if (
+            len(_Word) == 0 or 
+            len(_Pass) == 0 or 
+            len(_Word) != len(_Pass) or
+            _Pass.count(".") != 1
+        ):
+            return True
+        return False
+
+
 class Task_12_Resource(resources.ModelResource):
     class Meta:
         model = models.Task_12
 
-class Task_15_Resource(resources.ModelResource):
-    class Meta:
-        model = models.Task_15
+    
+    def skip_row(self, instance, original, row, import_validation_errors=None):
+        _Word = row["Word"]
+        _Pass = row["Pass"]
+        print(_Word, _Pass)
+        if (
+            len(_Word) == 0 or 
+            len(_Pass) == 0 or 
+            len(_Word) != len(_Pass) or
+            _Pass.count(".") != 1
+        ):
+            return True
+        return False
 
-# Ввод данных на странице
+
+# Импорт данных на странице
 class Task_9_Admin(ImportExportActionModelAdmin):
     resource_class = Task_9_Resource
+    list_display = ("Word", "Context_Before", "Pass", "Context_After")
+    search_fields = ("Word", "Context_Before", "Pass", "Context_After")
+
 
 class Task_10_Admin(ImportExportActionModelAdmin):
     resource_class = Task_10_Resource
+    list_display = ("Word", "Context_Before", "Pass", "Context_After")
+    search_fields = ("Word", "Context_Before", "Pass", "Context_After")
+
 
 class Task_11_Admin(ImportExportActionModelAdmin):
     resource_class = Task_11_Resource
+    list_display = ("Word", "Context_Before", "Pass", "Context_After")
+    search_fields = ("Word", "Context_Before", "Pass", "Context_After")
+
 
 class Task_12_Admin(ImportExportActionModelAdmin):
     resource_class = Task_12_Resource
-
-class Task_15_Admin(ImportExportActionModelAdmin):
-    resource_class = Task_15_Resource
+    list_display = ("Word", "Context_Before", "Pass", "Context_After")
+    search_fields = ("Word", "Context_Before", "Pass", "Context_After")
 
 
 # регистрация
@@ -47,4 +111,3 @@ admin.site.register(models.Task_9, Task_9_Admin)
 admin.site.register(models.Task_10, Task_10_Admin)
 admin.site.register(models.Task_11, Task_11_Admin)
 admin.site.register(models.Task_12, Task_12_Admin)
-admin.site.register(models.Task_15, Task_15_Admin)
