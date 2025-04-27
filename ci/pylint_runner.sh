d3006f3243fda6ed8e32b59abe48725ea822b017
Andrey Petrov <andrey.petrovich0880@gmail.com>
2025-04-27 21:30:37 +0300

    changed pylint command again

--- ci/pylint_runner.sh
+++ ci/pylint_runner.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+files="main/views.py main/views_api.py main/models.py main/forms.py training/views.py training/models.py"
+
 # run pylint
-pylint --disable="C0114, C0115, C0116" main/views.py main/views_api.py main/models.py main/forms.py training/views.py training/models.py | tee pylint.txt
+pylint --disable="C0114, C0115, C0116" $files | tee pylint.txt
 
 # get badge
 mkdir public
@@ -10,7 +12,7 @@ anybadge --value=$score --file=public/pylint.svg pylint
 echo "Pylint score was $score"
 
 # get html
-pylint --load-plugins=pylint_json2html $(ls -d */) --output-format=jsonextended > pylint.json
+pylint --load-plugins=pylint_json2html $files --output-format=jsonextended > pylint.json
 pylint-json2html -f jsonextended -o public/pylint.html pylint.json
 
 #cleanup
