747fb153e98ba5d70dc1d94eb81bf4d71ee3a6c7
Mat <matvey.meshcheryakov.j22@gmail.com>
2025-05-02 22:29:00 +0300

    added basic tests for registration

--- main/tests.py
+++ main/tests.py
@@ -1,22 +1,33 @@
 from django.test import TestCase, Client
-class BasicTest(TestCase):
-    # fixtures = ["db.json"]
+from django.urls import reverse
+
+from main.forms import UserRegistrationForm
+
+class RegistrationTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-        # self.client.login(username="root", password="root")
-    def test_main(self):
-        response = self.client.get('/')
+        self.response_url = reverse('registration')
+        self.response = {
+            'get': self.client.get(self.response_url),
+            'post': self.client.post(self.response_url)
+        }
+
+    def test_registration_page_get(self):
+        response = self.response['get']
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/accounts/registration.html')
 
-    def test_login(self):
-        response = self.client.get('/login/')
-        self.assertEqual(response.status_code,200)
+    def test_registration_form_get(self):
+        response = self.response['get']
+        user = response.context['user_form']
+        self.assertIsInstance(user, UserRegistrationForm)
 
-    # def test_logout(self):
-    #     response = self.client.get('/logout/')
-    #     self.assertEqual(response.status_code, 200)
+    def test_registration_form_post(self):
+        response = self.response['post']
+        user = response.context['user_form']
+        self.assertIsInstance(user, UserRegistrationForm)
 
-    def test_reg(self):
-        response = self.client.get('/registration/')
-        self.assertEqual(response.status_code, 200)
 
+
+
+    
