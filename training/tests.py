60937ddada99481893008e85bbbaa3320b9c3f31
Andrey Petrov <andrey.petrovich0880@gmail.com>
2025-05-09 15:19:15 +0300

    added new tests for taining

--- training/tests.py
+++ training/tests.py
@@ -1,6 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
 from django.contrib.auth.models import User
-from .models import Task9, Stats, Atts, MistakesAnswers
+from django.urls import reverse
+from training.models import Task9, Stats, Atts, MistakesAnswers
+from training.views import training, statistics_page, mistakes_page
+from training.serializers import TaskSerializer, WordsSerializer
+from main.models import Profile
 
 # Create your tests here.
 
@@ -79,3 +83,127 @@ class MistakesAnswersModelTest(TestCase):
         self.assertEqual(self.mistake.input_answer, "неправильный")
         self.assertEqual(self.mistake.correct_answer, "правильный")
         self.assertEqual(self.mistake.att, self.att)
+
+class ViewsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+        # Создаем или получаем профиль с активной подпиской для пользователя
+        self.profile, created = Profile.objects.get_or_create(
+            user=self.user,
+            defaults={'subscribe': True}
+        )
+        if not created:
+            self.profile.subscribe = True
+            self.profile.save()
+
+        self.client.login(username='testuser', password='testpass123')
+
+        # Создаем тестовые данные
+        self.stats = Stats.objects.create(
+            time=100,
+            successes=5,
+            mistakes=2,
+            user=self.user
+        )
+        self.att = Atts.objects.create(
+            time=50,
+            successes=3,
+            mistakes=1,
+            user=self.user
+        )
+        self.mistake = MistakesAnswers.objects.create(
+            input_answer="неправильный",
+            correct_answer="правильный",
+            att=self.att
+        )
+
+    def test_training_view(self):
+        # Тест для задания 9
+        response = self.client.get(reverse('task9'))
+        self.assertEqual(response.status_code, 200)
+
+        # Тест для несуществующего задания
+        response = self.client.get('/training/task999/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_statistics_page_view(self):
+        # Удаляем все существующие статистики для пользователя
+        Stats.objects.filter(user=self.user).delete()
+        # Создаем новую статистику
+        self.stats = Stats.objects.create(
+            time=100,
+            successes=5,
+            mistakes=2,
+            user=self.user
+        )
+
+        response = self.client.get(reverse('statistics'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/statistics/general_statistics.html')
+
+        # Проверяем контекст
+        self.assertEqual(response.context['time_all'], 100)
+        self.assertEqual(response.context['success_all'], 5)
+        self.assertEqual(response.context['mistakes_all'], 2)
+
+    def test_mistakes_page_view(self):
+        response = self.client.get(reverse('mistakes', args=[self.att.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/statistics/mistakes.html')
+
+        # Проверяем контекст
+        self.assertEqual(response.context['att_id'], self.att.id)
+        self.assertEqual(len(response.context['mistakes']), 1)
+
+        # Тест доступа к чужой попытке
+        other_user = User.objects.create_user(
+            username='otheruser',
+            password='testpass123'
+        )
+        other_att = Atts.objects.create(
+            time=30,
+            successes=2,
+            mistakes=1,
+            user=other_user
+        )
+        response = self.client.get(reverse('mistakes', args=[other_att.id]))
+        self.assertEqual(response.status_code, 404)
+
+
+class SerializersTest(TestCase):
+    def setUp(self):
+        self.task_data = {
+            'Word': 'тест',
+            'Context_Before': 'перед',
+            'Pass': 'те.т',
+            'Context_After': 'после'
+        }
+        self.tasks_data = [
+            self.task_data,
+            {
+                'Word': 'пример',
+                'Context_Before': 'до',
+                'Pass': 'пр.мер',
+                'Context_After': 'после'
+            }
+        ]
+
+    def test_task_serializer(self):
+        serializer = TaskSerializer(data=self.task_data)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data['Word'], 'тест')
+        self.assertEqual(serializer.validated_data['Context_Before'], 'перед')
+        self.assertEqual(serializer.validated_data['Pass'], 'те.т')
+        self.assertEqual(serializer.validated_data['Context_After'], 'после')
+
+    def test_words_serializer(self):
+        serializer = WordsSerializer(data=self.tasks_data, many=True)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(len(serializer.validated_data), 2)
+        self.assertEqual(serializer.validated_data[0]['Word'], 'тест')
+        self.assertEqual(serializer.validated_data[1]['Word'], 'пример')
