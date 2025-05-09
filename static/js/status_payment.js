f29f2c3427b42539cec8c3ad36bd1f1b7fae4c4d
Andrey Petrov <andrey.petrovich0880@gmail.com>
2025-05-09 20:38:46 +0300

    deleted static

--- static/js/status_payment.js
+++
@@ -1,4 +0,0 @@
-console.log("status")
-const now = new Date();
-document.getElementById("transactionDate").textContent =
-    now.toLocaleDateString("ru-RU") + " " + now.toLocaleTimeString("ru-RU");
