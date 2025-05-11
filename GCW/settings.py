96b22280256abcba4cf3f15e075c8ca17a963821
Andrey Petrov <andrey.petrovich0880@gmail.com>
2025-05-11 13:52:02 +0300

    changed files to develop version

--- GCW/settings.py
+++ GCW/settings.py
@@ -1,5 +1,4 @@
 import os
-from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
